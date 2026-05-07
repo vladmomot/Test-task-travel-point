cd1918a379f445a62b48c2f2a25e9d3013791319
Developer <momotv678@gmail.com>
2026-05-07 23:27:34 +0300

    add env

---
+++ tmdb-movie-search/src/shared/api/tmdb/config/tmdb.ts
@@ -0,0 +1,4 @@
+export const TMDB_BASE_URL = import.meta.env.VITE_TMDB_BASE_URL
+export const TMDB_IMAGE_BASE_URL = import.meta.env.VITE_TMDB_IMAGE_BASE_URL
+export const TMDB_API_KEY = import.meta.env.VITE_TMDB_API_KEY
+export const TMDB_JWT_TOKEN = import.meta.env.VITE_TMDB_JWT_TOKEN
